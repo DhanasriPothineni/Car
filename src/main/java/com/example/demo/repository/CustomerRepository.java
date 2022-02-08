810f7253aa5e25b02ee40370323ca00292c153a6
Dhanasri <dhanasreepothineni@gmail.com>
2022-02-08 17:50:43 +0530

    Final commit

---
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer>{
+
+	Iterable<Customer> findAll();
+
+}
